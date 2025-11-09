f3f600f5595172cfca1626e8645038037e039e7d
dpierse <x25174142@student.ncirl.ie>
2025-11-09 16:32:18 +0000

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,20 @@
+  document.getElementById("inputForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let user1 = document.getElementById("user1").value;
+    let user2 = document.getElementById("user2").value;
+    let result;
+    
+	if (user1 === user2) {
+      result = "It's a draw!";
+    } else if (
+      (user1 === "rock" && user2 === "scissors") ||
+      (user1 === "paper" && user2 === "rock") ||
+      (user1 === "scissors" && user2 === "paper")
+    ) {
+      result = "User 1 wins! Congratulations User 1";
+    } else {
+      result = "User 2 wins! Congratulations User 2";
+    }
+
+    alert(result);
+  });
